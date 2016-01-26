ca50833139434ad358e5d8081b8cea832cbf6322
knolegsb <seon2you@hotmail.com>
2016-01-25 16:26:37 -0800

    property injection

---
+++ DependencyPropertyInjection/Client.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DependencyPropertyInjection
+{
+    public class Client
+    {
+        private IService _service;
+
+        public IService Service
+        {
+            set { this._service = value; }
+        }
+
+        public void Start()
+        {
+            Console.WriteLine("Service Started");
+            this._service.Serve();
+        }
+    }
+}
